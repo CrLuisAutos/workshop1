fa64f75795666b55df5e37691e358115a8d38b87
CrLuisAutos <espinozaluis3680@gmail.com>
2017-06-12 20:07:21 -0600

    Add files via upload

---
+++ crudasp/crudasp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace crudasp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ crudasp/crudasp/Models/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crudasp.Models
+{
+    public class Carro
+    {
+        public int id{ get; set; }
+        public String nombre{ get; set; }
+        public String apellido { get; set; }
+    }
+}
